84e7d4fbb20f50f2f56b2e813642ee71ddeba3fb
abdou shady <modmoney7@gmail.com>
2019-01-18 20:56:35 +0100

    added events listenner to the js file

--- app.js
+++ app.js
@@ -15,6 +15,37 @@ var UiController = (function(){
 
 var controller = (function(one,two){
     
+    var addItem = function(){
+        
+        
+      
+        
+        
+        
+        
+        
+    }
+    
+    
+    document.getElementById('valider').addEventListener('click',addItem);
+    
+    
+    
+    document.addEventListener('keypress',function(e){
+        
+        if (e.keyCode == 13){
+            
+            
+            
+        }
+        
+        
+        
+    });
+    
+    
+    
+    
     
     
 })(BudgetController,UiController);
